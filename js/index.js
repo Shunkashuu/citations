af17a778cf1d0f0916909bfc8fb5da4f26150965
Sora <sora78700@gmail.com>
2020-06-15 16:09:33 +0200

    clean js and css
    clean js and css + shadow card before working on loop and number of phrases

--- js/index.js
+++ js/index.js
@@ -1,18 +1,23 @@
-//appel des fichiers json
+//appel des fichiers json :
 const fr = ('js/citations.json')
 const en = ('js/quotes.json')
 
-// variables pour récupérer depuis le fichier html les composants :
-let sujet = ""
-let verbe= ""
+//création de variables :
+let verbe = ""
 let nom = ""
+
+// variables pour récupérer depuis le fichier html les composants :
 let btn = document.getElementById('btn')
 let phr = document.getElementById('phrase')
-let number = document.getElementById('number')
-let citations = []  //mettre les données dans un tableau array 
+let number = document.getElementById('number').value
+
+//mettre les données dans un tableau array :
+let citations = []  
 
 // événément du bouton fr :
-btn.addEventListener('click', updatePage)
+for (let x = 0; x < number; x++) {
+    btn.addEventListener('click', updatePage)
+}    
 
 //if button fr
 
@@ -20,76 +25,50 @@ btn.addEventListener('click', updatePage)
 fetch(fr).then((data) => {
     data.json().then((data) => {
         citations = data
-
-        //affichage du fichier citations.json :
-        console.log(citations) 
-        
     })
 })
 
 //if btn en
 
 // événément du bouton en :
-btnen.addEventListener('click', quotes)
+for (let x = 0; x < number; x++) {
+    btnen.addEventListener('click', quotes)
+}
 
+//récupérer toutes les citations depuis le lien json :
 fetch(en).then((data) => {
     data.json().then((data) => {
         quotes = data
-
-        //affichage du fichier quotes.json :
-        console.log(quotes) 
-        
     })
 })
 
-
-
 //bouton fr :
 function updatePage() {
-    //for(i=1,i<=nbcitation,i++)
-
-    //choix au hasard d'une citation parmis la liste :
-    let random = Math.floor(Math.random() * (citations.length - 0))
-    let randomSujet = citations[random]
-    let random2 = Math.floor(Math.random() * (citations.length - 0))
-    let randomVerbe = citations[random2]
-    let random3 = Math.floor(Math.random() * (citations.length - 0))
-    let randomNom = citations[random3]
-
-    console.log(randomSujet)
-
-    //modifie le contenu de la balise + concaténation :
-    phr.innerText=randomSujet['sujet']+" "+randomVerbe['verbe']+" "+randomNom['nom']
+        //for(i=1,i<=nbcitation,i++)
+    
+        //choix au hasard d'une citation parmis la liste :
+        let random = Math.floor(Math.random() * (citations.length - 0))
+        let randomSujet = citations[random]
+        let random2 = Math.floor(Math.random() * (citations.length - 0))
+        let randomVerbe = citations[random2]
+        let random3 = Math.floor(Math.random() * (citations.length - 0))
+        let randomNom = citations[random3]
+        //modifie le contenu de la balise + concaténation :
+        phr.innerText = randomSujet['sujet'] + " " + randomVerbe['verbe'] + " " + randomNom['nom']
 }
 
-//bouton english :
+//bouton en :
 function quotes() {
-    //for(i=1,i<=nbcitation,i++)
+        //for(i=1,i<=nbcitation,i++)
 
-    let random = Math.floor(Math.random() * (quotes.length - 0))
-    let randomSujet = quotes[random]
-    let random2 = Math.floor(Math.random() * (quotes.length - 0))
-    let randomVerbe = quotes[random2]
-    let random3 = Math.floor(Math.random() * (quotes.length - 0))
-    let randomNom = quotes[random3]
-
-    console.log(randomSujet)
-
-    phr.innerText=randomSujet['sujet']+" "+randomVerbe['verbe']+" "+randomNom['nom']
+        // for (let x = 0; x < number; x++) {
+        let random = Math.floor(Math.random() * (quotes.length - 0))
+        let randomSujet = quotes[random]
+        let random2 = Math.floor(Math.random() * (quotes.length - 0))
+        let randomVerbe = quotes[random2]
+        let random3 = Math.floor(Math.random() * (quotes.length - 0))
+        let randomNom = quotes[random3]
+        //modifie le contenu de la balise + concaténation :
+        phr.innerText = randomSujet['sujet'] + " " + randomVerbe['verbe'] + " " + randomNom['nom']
+    //}
 }
-
-//nettois les phrases avant d'en gÃ©nÃ©rer de nouvelles
-phrase = document.querySelector("phrase").value;        //reprend le theme choisis aprÃ¨s le click
-    phrase = document.getElementById("number").value;   //reprend le nombre choisis aprÃ¨s le click
-    while (importSentence.firstChild) {
-        importSentence.removeChild(importSentence.firstChild);
-} 
-
-
-//boucle pour générer le bon nombre de phrases :
-for (let x = 0; x < number; x++) {
-    let space = document.createElement("br");
-    let newSentence = document.createTextNode(randomSentence());
-    importSentence.appendChild(newSentence);    //imprime la nouvelle phrase
-    importSentence.appendChild(space);          //imprime un retour à  la ligne
-}   
