bb7d5011e19df686ba86b06eaa6dc8ab503006e1
Sora <sora78700@gmail.com>
2020-06-19 18:59:44 +0200

    add last features
    Ajout dans le js de la fonction pour afficher le bon nombre de phrases séléctionnées et clear les autres phrases en rafraichissant

--- js/index.js
+++ js/index.js
@@ -8,67 +8,131 @@ let nom = ""
 
 // variables pour récupérer depuis le fichier html les composants :
 let btn = document.getElementById('btn')
+let btnen = document.getElementById('btnen')
 let phr = document.getElementById('phrase')
-let number = document.getElementById('number').value
 
 //mettre les données dans un tableau array :
 let citations = []  
 
 // événément du bouton fr :
-for (let x = 0; x < number; x++) {
-    btn.addEventListener('click', updatePage)
-}    
+    btn.addEventListener('click', function() {
+        //actualise le nombre de phrase à écrire
+        let number = document.getElementById("number").value
+    
+        //efface le contenu de la balise texte
+        while(phr.firstChild){
+            phr.removeChild(phr.firstChild)
+        }
 
-//if button fr
+        //message d'erreur si le nombre entré n'est pas entre 1 et 5
+        if (number > 5 || number <= 0 ){
+            let erreur = "Je ne peux génerer que 5 phrases maximum ! Ma créatrice n'était pas très douée pour que j'en fasse plus :D"
+            let phrase = document.createTextNode(erreur)
+            phr.appendChild(phrase)
+        }
+        else { //boucle pour afficher chacune des phrases  
+            for (let test = 0; test < number; test++) {
 
-//récupérer toutes les citations depuis le lien json :
+                //création variable contenant phrase et saut de ligne
+                let saut = document.createElement("br")
+                let phrase = document.createTextNode(Random_phrase())
+
+                //ajoute le texte de la variable
+                phr.appendChild(phrase)
+
+                //ajoute un <br> pour le saut de ligne entre les phrases
+                phr.appendChild(saut)
+            }
+        }    
+    })
+
+
+//récupérer toutes les citations depuis le lien json fr
 fetch(fr).then((data) => {
     data.json().then((data) => {
         citations = data
     })
 })
 
-//if btn en
-
-// événément du bouton en :
-for (let x = 0; x < number; x++) {
-    btnen.addEventListener('click', quotes)
+//fonction random fr :
+function Random_phrase() {
+                       
+    //choix au hasard d'une citation parmis la liste :
+    let random = Math.floor(Math.random() * (citations.length - 0))
+    let randomSujet = citations[random]
+    let random2 = Math.floor(Math.random() * (citations.length - 0))
+    let randomVerbe = citations[random2]
+    let random3 = Math.floor(Math.random() * (citations.length - 0))
+    let randomNom = citations[random3]
+    
+    //concaténation :
+    let phrase_random= randomSujet['sujet'] + " " + randomVerbe['verbe'] + " " + randomNom['nom']
+    
+    //Test de l'affichage de phrase dans la console
+    console.log(phrase_random)
+    
+    //Renvoie la phrase randomisée
+    return phrase_random
 }
 
-//récupérer toutes les citations depuis le lien json :
+
+
+// événément du bouton en :
+btnen.addEventListener('click', function() {
+    //actualise le nombre de phrase à écrire
+    let number = document.getElementById("number").value
+
+    //efface le contenu de la balise texte
+    while(phr.firstChild){
+        phr.removeChild(phr.firstChild)
+    }
+
+    //message d'erreur si le nombre entré n'est pas entre 1 et 5
+    if (number > 5 || number <= 0 ){
+        let erreur = "Je ne peux génerer que 5 phrases maximum ! Ma créatrice n'était pas très douée pour que j'en fasse plus :D"
+        let phrase = document.createTextNode(erreur)
+        phr.appendChild(phrase)
+    }
+    else { //boucle pour afficher chacune des phrases  
+        for (let test = 0; test < number; test++) {
+
+            //création variable contenant phrase et saut de ligne
+            let saut = document.createElement("br")
+            let phrase = document.createTextNode(Random_quotes())
+
+            //ajoute le texte de la variable
+            phr.appendChild(phrase)
+
+            //ajoute un <br> pour le saut de ligne entre les phrases
+            phr.appendChild(saut)
+        }
+    }    
+})
+
+//récupérer toutes les citations depuis le lien json en :
 fetch(en).then((data) => {
     data.json().then((data) => {
         quotes = data
     })
 })
 
-//bouton fr :
-function updatePage() {
-        //for(i=1,i<=nbcitation,i++)
-    
-        //choix au hasard d'une citation parmis la liste :
-        let random = Math.floor(Math.random() * (citations.length - 0))
-        let randomSujet = citations[random]
-        let random2 = Math.floor(Math.random() * (citations.length - 0))
-        let randomVerbe = citations[random2]
-        let random3 = Math.floor(Math.random() * (citations.length - 0))
-        let randomNom = citations[random3]
-        //modifie le contenu de la balise + concaténation :
-        phr.innerText = randomSujet['sujet'] + " " + randomVerbe['verbe'] + " " + randomNom['nom']
-}
-
 //bouton en :
-function quotes() {
-        //for(i=1,i<=nbcitation,i++)
-
-        // for (let x = 0; x < number; x++) {
-        let random = Math.floor(Math.random() * (quotes.length - 0))
-        let randomSujet = quotes[random]
-        let random2 = Math.floor(Math.random() * (quotes.length - 0))
-        let randomVerbe = quotes[random2]
-        let random3 = Math.floor(Math.random() * (quotes.length - 0))
-        let randomNom = quotes[random3]
-        //modifie le contenu de la balise + concaténation :
-        phr.innerText = randomSujet['sujet'] + " " + randomVerbe['verbe'] + " " + randomNom['nom']
-    //}
+function Random_quotes() {
+                       
+    //choix au hasard d'une citation parmis la liste :
+    let random = Math.floor(Math.random() * (quotes.length - 0))
+    let randomSujet = quotes[random]
+    let random2 = Math.floor(Math.random() * (quotes.length - 0))
+    let randomVerbe = quotes[random2]
+    let random3 = Math.floor(Math.random() * (quotes.length - 0))
+    let randomNom = quotes[random3]
+    
+    //concaténation :
+    let phrase_random= randomSujet['sujet'] + " " + randomVerbe['verbe'] + " " + randomNom['nom']
+    
+    //Test de l'affichage de phrase dans la console
+    console.log(phrase_random)
+    
+    //Renvoie la phrase randomisée
+    return phrase_random
 }
